2b7eaa2eaef736b75ad8116ecafcb6103fa48ae6
Hassan Timli <htimli@emi.u-bordeaux.fr>
2021-11-24 16:14:00 +0100

    add first component

--- dev/frontend/my-app/src/app/app.module.ts
+++ dev/frontend/my-app/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+
+import  {RouterModule, Routes} from '@angular/router' ;
+
+
+const appRoutes : Routes = [
+  { path: 'homepage', component : HomePageComponent } 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignInPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
