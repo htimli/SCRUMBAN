23139429d01f2cbe3228ecc417bba78722e3a866
Hassan Timli <htimli@emi.u-bordeaux.fr>
2021-12-08 13:38:29 +0100

    add contrloller sprint

--- dev/backend/src/routes/index.js
+++ dev/backend/src/routes/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 
 const usersRoutes = require("../routes/users");
 const projectsRoutes = require("../routes/projects");
+const sprintsRoutes = require('../routes/sprints');
 
 
 router.use('/users', usersRoutes);
 router.use('/projects',projectsRoutes);
+router.use('/sprints',sprintsRoutes);
 
 router.get('/',function (req, res, next){ 
     res.status(200).json({
