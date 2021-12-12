9e156a4b18641a4a27499544308cdf95f19dd8b8
Saliou <dmsaliou9292@gmail.com>
2021-12-12 16:32:47 +0100

    update 2

--- dev/frontend/my-app/src/app/new-sprint/new-sprint.component.ts
+++ dev/frontend/my-app/src/app/new-sprint/new-sprint.component.ts
@@ -7,73 +7,46 @@ import { ProjectsService } from '../services/projects.service';
 @Component({
   selector: 'app-new-sprint',
   templateUrl: './new-sprint.component.html',
-  styleUrls: ['./new-sprint.component.css']
+  styleUrls: ['./new-sprint.component.css'],
 })
 export class NewSprintComponent implements OnInit {
-
-  
-  projectId :'';
-
-  
+  projectId: '';
 
   sprintData = {
-    name : '',
-    tasks : []
-  }
-  
-  
+    name: '',
+    tasks: [],
+  };
 
-  
-  
-  @ViewChild(ProductBacklogComponent, {static :true}) hijo : ProductBacklogComponent 
+  @ViewChild(ProductBacklogComponent, { static: true })
+  hijo: ProductBacklogComponent;
 
-
-  constructor(private router: Router ,private projectService : ProjectsService) { }
+  constructor(
+    private router: Router,
+    private projectService: ProjectsService
+  ) {}
 
   ngOnInit(): void {
-<<<<<<< HEAD
-  }
-
-  onSubmit(form: NgForm) {
-    
-  }
-
-  onNewSprint(form:NgForm) {
-  this.sprintData.name = form.value.SprintName;
-
-  this.projectService.addProjectSprint(this.projectService.currentProject,this.projectService.currentProject._id,this.sprintData).then(
-    () => {this.router.navigate(['project'])},
-    () => {console.log("err");}
-    );
-  }
-
-=======
     this.projectId = this.projectService.currentProject._id;
   }
 
-
-  onAdd(){
+  onAdd() {
     this.sprintData.tasks.push(this.hijo.getTaskSelected());
   }
 
+  onNewSprint(form: NgForm) {
+    this.sprintData.name = form.value.SprintName;
 
-  onNewSprint(form:NgForm) {
-  this.sprintData.name = form.value.SprintName;
-  
-  console.log('this.sprintData',this.sprintData);
+    console.log('this.sprintData', this.sprintData);
 
-  this.projectService.addProjectSprint(this.projectService.currentProject,this.sprintData)
-  .then(
-    () => {this.router.navigate(['project'])},
-    () => {console.log("err");}
-  );
-  
+    this.projectService
+      .addProjectSprint(this.projectService.currentProject, this.sprintData)
+      .then(
+        () => {
+          this.router.navigate(['project']);
+        },
+        () => {
+          console.log('err');
+        }
+      );
   }
-
-  
-
->>>>>>> bf666bbd5e737941f355c022fd4c60f030cf6151
-
-  
-
 }
--- dev/frontend/my-app/src/app/new-task/new-task.component.ts
+++ dev/frontend/my-app/src/app/new-task/new-task.component.ts
@@ -1,54 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-=======
 import { ProjectsService } from '../services/projects.service';
->>>>>>> bf666bbd5e737941f355c022fd4c60f030cf6151
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrls: ['./new-task.component.css']
+  styleUrls: ['./new-task.component.css'],
 })
 export class NewTaskComponent implements OnInit {
-
-<<<<<<< HEAD
-  constructor(private router: Router) { }
-=======
   taskData = {
-    title : '',
-    desc  : ''
-  }
+    title: '',
+    desc: '',
+  };
 
-  constructor(private router: Router ,private projectService : ProjectsService) { }
->>>>>>> bf666bbd5e737941f355c022fd4c60f030cf6151
+  constructor(
+    private router: Router,
+    private projectService: ProjectsService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {}
 
-  }
-
-<<<<<<< HEAD
-  onNewTask(){
-    this.router.navigate(['project'])
-=======
-  onNewTask(form:NgForm){
-
+  onNewTask(form: NgForm) {
     this.taskData.title = form.value.TaskName;
     this.taskData.desc = form.value.TaskDesc;
 
-    this.projectService.addProjectTask(this.projectService.currentProject,this.taskData)
-    .then(
-      () => {this.router.navigate(['project'])},
-      () => {console.log("err");}
-    );
-
-
-    
->>>>>>> bf666bbd5e737941f355c022fd4c60f030cf6151
+    this.projectService
+      .addProjectTask(this.projectService.currentProject, this.taskData)
+      .then(
+        () => {
+          this.router.navigate(['project']);
+        },
+        () => {
+          console.log('err');
+        }
+      );
   }
-
 }
